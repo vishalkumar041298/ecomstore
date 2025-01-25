8e7d123868f5d3a3ede616590e780f953a99b0df
Vishal <vishallindan170@gmail.com>
2025-01-25 18:10:45 +0000

    modfied

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'stores.views.categories'
             ],
         },
     },
@@ -118,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'static/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- store/models.py
+++ store/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
+    # category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100, default='un-branded')
+    slug = models.SlugField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', blank=True)
+
+    class Meta:
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.title
