f05a2c903ad0351bf33a555dc2a37d35177102a2
Vishal <vishallindan170@gmail.com>
2025-01-31 19:43:32 +0000

    s

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ecomstore-sbr3.onrender.com', '*']
 
-# CSRF_TRUSTED_ORIGINS = ['']
+CSRF_TRUSTED_ORIGINS = ['https://ecomstore-sbr3.onrender.com']
 
 # Application definition
 
